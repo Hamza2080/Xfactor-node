18df199f475f4aaa233c9e94e7db6a39ebd27ff2
HamzaPLT <Hamza.Javed@prettylittlething.com>
2020-07-08 10:09:13 +0500

    user authentication in nest application

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Post, Body, UsePipes, ValidationPipe, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, ValidationPipe, Get, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user-service/user.service';
 import { UserDTO, UserRO } from './user.dto';
+import { AuthGuard } from 'src/shared/auth.guard';
+import { User } from './user.decorator';
 
 @Controller('user')
 export class UserController {
 
     constructor(private userService: UserService) {}
+
+    // @Get('/showall')
+    // @UseGuards(new AuthGuard())
+    // showall(@User() user){
+    //     console.log(user);
+
+    // }
+
+
     @Get('findById/:id')
+    @UseGuards(new AuthGuard())
     findUserById(@Param('id') id: string) {
         return this.userService.findById(id);
     }
